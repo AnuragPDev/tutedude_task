d1c3980b9fc2d8674c7c52fc9b0b8e70755feee7
Anurag Pant <anuragp.dev@gmail.com>
2025-04-22 09:24:00 +0000

    Task one uploaded

---
+++ task1.py
@@ -0,0 +1,17 @@
+# Problem Statement: Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+# 3.  Displays the results of each operation on the screen.
+#  Expected Output:
+# The output should include the result of each operation performed, for example:
+ 
+num1 = int(input("Enter the first number: "))
+num2= int(input("enter the second number: "))
+print("Addition: ", num1+num2)
+print("Subtraction: ", num1-num2)
+print("Multiplication: ", num1*num2)
+print("Division: ", num1/num2)
